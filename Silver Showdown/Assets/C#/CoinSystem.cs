b535ecd0575d03afbe6a0e868f4ab71f37d8edce
shefrum <haykaakaramyan@gmail.com>
2023-08-19 17:44:01 +0400

    marcet code

--- Silver Showdown/Assets/C#/CoinSystem.cs
+++ Silver Showdown/Assets/C#/CoinSystem.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinSystem : MonoBehaviour
 {
     public int Coin;
+    [SerializeField] TextMeshProUGUI Coin_Text;
 
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        Coin_Text.text = Coin.ToString();
     }
 }
--- Silver Showdown/Assets/C#/Market.cs
+++ Silver Showdown/Assets/C#/Market.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class Market : MonoBehaviour
 {
-    [SerializeField] GameObject[] Hp;
     public CoinSystem coinSystem;
+
+    [Header("HP")]
+    [SerializeField] GameObject[] Hp;
     [SerializeField] private GameObject Hp_Saled;
+    [SerializeField] private GameObject Hp_Buy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Shield")]
+    [SerializeField] GameObject[] Shield;
+    [SerializeField] private GameObject Shield_Saled;
+    [SerializeField] private GameObject Shield_Buy;
+
+
+    [Header("Gun")]
+    [SerializeField] GameObject[] Gun;
+    [SerializeField] private GameObject Gun_Saled;
+    [SerializeField] private GameObject Gun_Buy;
 
     int i_Hp = 0;
     public void HP_buy()
@@ -30,11 +33,50 @@ public class Market : MonoBehaviour
             coinSystem.Coin--;
         }
 
-        if(i_Hp == Hp.Length)
+        if (i_Hp == Hp.Length)
         {
-            this.gameObject.SetActive(false);
-            Hp_Saled.SetActive(false);
+            Hp_Buy.SetActive(false);
+            Hp_Saled.SetActive(true);
         }
 
     }
+
+    int i_Shield = 0;
+    public void Shield_buy()
+    {
+        if (coinSystem.Coin > 0)
+        {
+            Shield[i_Shield].gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            i_Shield++;
+            coinSystem.Coin--;
+        }
+
+        if(i_Shield == Shield.Length)
+        {
+            Shield_Buy.SetActive(false);
+            Shield_Saled.SetActive(true);
+        }
+
+    }
+
+
+    int i_Gun = 0;
+    public void Gun_buy()
+    {
+        if (coinSystem.Coin > 0)
+        {
+            Gun[i_Gun].gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            i_Gun++;
+            coinSystem.Coin--;
+        }
+
+        if (i_Gun == Gun.Length)
+        {
+            Gun_Buy.SetActive(false);
+            Gun_Saled.SetActive(true);
+        }
+
+    }
+
+
 }
